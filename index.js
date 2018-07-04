56e1f0bcff9f3c57f6286176944c04400e847708
FutureScript <fernandobaxter1@gmail.com>
2018-07-04 03:56:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ function logWhisper (string) {
 
 
 function sayHiToGrandma (string) {
-  if ()
-  return "I can't hear you!" === string.toLowerCase()
+  if (string.toLowerCase() === string) {
+  return "I can't hear you!" }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
 }
